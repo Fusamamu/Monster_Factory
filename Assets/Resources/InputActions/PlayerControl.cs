ba1e3ec5992ab02867ed02a8cdaebaf6abdcc064
Natthapoom <natthapoom654@outlook.com>
2023-10-18 00:09:13 +0700

    Change Player Control key

--- Assets/Resources/InputActions/PlayerControl.cs
+++ Assets/Resources/InputActions/PlayerControl.cs
@@ -37,6 +37,24 @@ namespace Monster
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""ChangePlayerIndexUp"",
+                    ""type"": ""Button"",
+                    ""id"": ""5f1f187a-a4ac-48fa-a658-e66a9e09a717"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""ChangePlayerIndexDown"",
+                    ""type"": ""Button"",
+                    ""id"": ""d8b157b5-b083-4f55-9566-2443eb0730fb"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -50,6 +68,28 @@ namespace Monster
                     ""action"": ""ChangePlayerControl"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""506afc00-fe4d-4fc1-aeda-69d40a73a813"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ChangePlayerIndexUp"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""df66d207-3af2-407c-99d2-453d7e93fccf"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ChangePlayerIndexDown"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -59,6 +99,8 @@ namespace Monster
             // PlayerSelection
             m_PlayerSelection = asset.FindActionMap("PlayerSelection", throwIfNotFound: true);
             m_PlayerSelection_ChangePlayerControl = m_PlayerSelection.FindAction("ChangePlayerControl", throwIfNotFound: true);
+            m_PlayerSelection_ChangePlayerIndexUp = m_PlayerSelection.FindAction("ChangePlayerIndexUp", throwIfNotFound: true);
+            m_PlayerSelection_ChangePlayerIndexDown = m_PlayerSelection.FindAction("ChangePlayerIndexDown", throwIfNotFound: true);
         }
 
         public void Dispose()
@@ -119,11 +161,15 @@ namespace Monster
         private readonly InputActionMap m_PlayerSelection;
         private IPlayerSelectionActions m_PlayerSelectionActionsCallbackInterface;
         private readonly InputAction m_PlayerSelection_ChangePlayerControl;
+        private readonly InputAction m_PlayerSelection_ChangePlayerIndexUp;
+        private readonly InputAction m_PlayerSelection_ChangePlayerIndexDown;
         public struct PlayerSelectionActions
         {
             private @PlayerControl m_Wrapper;
             public PlayerSelectionActions(@PlayerControl wrapper) { m_Wrapper = wrapper; }
             public InputAction @ChangePlayerControl => m_Wrapper.m_PlayerSelection_ChangePlayerControl;
+            public InputAction @ChangePlayerIndexUp => m_Wrapper.m_PlayerSelection_ChangePlayerIndexUp;
+            public InputAction @ChangePlayerIndexDown => m_Wrapper.m_PlayerSelection_ChangePlayerIndexDown;
             public InputActionMap Get() { return m_Wrapper.m_PlayerSelection; }
             public void Enable() { Get().Enable(); }
             public void Disable() { Get().Disable(); }
@@ -136,6 +182,12 @@ namespace Monster
                     @ChangePlayerControl.started -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerControl;
                     @ChangePlayerControl.performed -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerControl;
                     @ChangePlayerControl.canceled -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerControl;
+                    @ChangePlayerIndexUp.started -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexUp.performed -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexUp.canceled -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexDown.started -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexDown;
+                    @ChangePlayerIndexDown.performed -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexDown;
+                    @ChangePlayerIndexDown.canceled -= m_Wrapper.m_PlayerSelectionActionsCallbackInterface.OnChangePlayerIndexDown;
                 }
                 m_Wrapper.m_PlayerSelectionActionsCallbackInterface = instance;
                 if (instance != null)
@@ -143,6 +195,12 @@ namespace Monster
                     @ChangePlayerControl.started += instance.OnChangePlayerControl;
                     @ChangePlayerControl.performed += instance.OnChangePlayerControl;
                     @ChangePlayerControl.canceled += instance.OnChangePlayerControl;
+                    @ChangePlayerIndexUp.started += instance.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexUp.performed += instance.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexUp.canceled += instance.OnChangePlayerIndexUp;
+                    @ChangePlayerIndexDown.started += instance.OnChangePlayerIndexDown;
+                    @ChangePlayerIndexDown.performed += instance.OnChangePlayerIndexDown;
+                    @ChangePlayerIndexDown.canceled += instance.OnChangePlayerIndexDown;
                 }
             }
         }
@@ -150,6 +208,8 @@ namespace Monster
         public interface IPlayerSelectionActions
         {
             void OnChangePlayerControl(InputAction.CallbackContext context);
+            void OnChangePlayerIndexUp(InputAction.CallbackContext context);
+            void OnChangePlayerIndexDown(InputAction.CallbackContext context);
         }
     }
 }
